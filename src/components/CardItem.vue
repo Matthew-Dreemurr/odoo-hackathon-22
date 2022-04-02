c0782efeea9748be5314ee7051f1b7239194bf27
matthew-dreemurr <hadadmat@gmail.com>
2022-04-03 00:38:55 +0200

    update

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,104 +1,125 @@
 <script>
-export default
-{
+export default {
   props: {
     icon: {
       type: String,
       required: true,
-      default: "https://picsum.photos/128",
+      default: "https://www.pikpng.com/pngl/m/105-1052172_png-file-svg-transparent-background-education-icon-clipart.png",
     },
     title: {
       type: String,
-      default: "Title lorem ipsum"
+      default: "Title lorem ipsum",
     },
     description: {
       type: String,
-      default:
-      `
+      default: `
       Description, lorem ipsum dolor sit amet brown fox jumped over the lazy dog. <br>
       <br>This <b>should</b> be on a new line <br>
       <br>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis laoreet arcu augue, eget consectetur urna bibendum ac. Sed ac augue at est tincidunt sagittis sed id est. Nam feugiat arcu sem, vitae congue lacus lacinia eget. Vestibulum sit amet tempor turpis. Proin non posuere diam. Suspendisse facilisis tempor sodales. Aliquam erat volutpat. Suspendisse vehicula arcu mauris, nec mollis arcu vulputate sit amet. Mauris vitae nisi a velit fermentum interdum ac vel massa. Morbi ante risus, blandit non odio at, suscipit ornare libero. Suspendisse potenti. Vestibulum eget nibh augue. Aliquam erat volutpat.
+      Lorem ipsum dolor sit ametpat.
 `,
     },
     tags_array: {
       type: Array,
       default: [
-        	{
-            text: "this is a tag #1",
-           style: "background-color: #0088FF;"},
-        	{
-            text: "this is another tag #2",
-           style: "background-color:#00DD44;"},
-        	{
-            text: "and another one #3",
-           style: "background-color: #FF4400;"},
-        	{
-            text: "and a very long tag : lorem ipsum dolor sit amet #4",
-           style: "background-color: #FF0000;"},
+        {
+          text: "this is a tag #1",
+          style: "background-color: #0088FF;",
+        },
+        {
+          text: "this is another tag #2",
+          style: "background-color:#00DD44;",
+        },
+        {
+          text: "and another one #3",
+          style: "background-color: #FF4400;",
+        },
+        {
+          text: "and a very long tag : lorem ipsum dolor sit amet #4",
+          style: "background-color: #FF0000;",
+        },
       ],
-    }
+    },
   },
-  data(){
-    return {
-
-    }
-  }
-}
+  data() {
+    return {};
+  },
+};
 </script>
 
 <template>
   <div class="card-div">
     <div class="title-div">
-      <img class="module-icon" :src="icon" alt="Module Icon">
+      <img class="module-icon" :src="icon" alt="Module Icon" />
       <h1 class="title">{{ title }}</h1>
     </div>
     <p class="description"><span v-html="description"></span></p>
-    <li class="tags-div" >
-      <ul class="tag" v-for="tag in tags_array" :key="tag.text" :style="tag.style">{{ tag.text }}</ul>
+    <li class="tags-div">
+      <ul
+        class="tag"
+        v-for="tag in tags_array"
+        :key="tag.text"
+        :style="tag.style"
+      >
+        {{
+          tag.text
+        }}
+      </ul>
     </li>
   </div>
 </template>
 
 <style scoped>
-
-  *- {
-    border: blue 0.2px dotted;
-  }
-
 .card-div {
-  margin: 2em;
+  color: #dddddd;
+  margin: 1em;
   padding: 1em;
+  width: 100%;
+  min-width: 25vw;
+  max-width: vw;
+
+  font-size: small;
+
   border-radius: 1em;
-  border: black solid 1px;
-  background-color: white;
 
-  box-shadow: 0 0 1em 0.1em #000000;
+  background-color: #222222;
 
+  box-shadow: 0 0.2em 2em 0.01em #00000044;
+
+  flex: 1 1;
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
 
-  transition: transform 0.2s;
+  transition: .3s transform cubic-bezier(.155,1.105,.295,1.12);
+  transform: scale(0.99);
+
+  cursor: pointer;
 }
 
-  .title-div {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: flex-start;
-  }
+.title-div {
+  padding: 1em;
+  padding-left: 0;
+  width: 100%;
+  display: flex;
+  flex-wrap: no-wrap;
+  align-items: center;
+  justify-content: flex-start;
+}
 
-  .description {
-    padding: 1em;
-  	box-shadow: 0 0 0.5em 0.01em #000000;
-    border-radius: 1em;
-  }
+.description {
+  padding: 1em;
+  box-shadow: 0 0 0.5em 0.01em #00000066;
+  border-radius: 1em;
+}
 
 img {
   display: inline-block;
+  filter: brightness(100%);
   margin: 1em;
-  border-radius: 1em;
-  }
+  border-radius: 0.2em;
+  width: 3em;
+}
 
 h1 {
   display: inline-block;
@@ -113,22 +134,27 @@ h1 {
   flex-wrap: wrap;
 }
 
-.tags-div, .tag{
+.tags-div,
+.tag {
   list-style-type: none;
 }
 
-  .tag {
-    display: inline-block;
-  	margin: 0.2em;
-    padding: 0.2em 0.8em;
-    text-transform: uppercase;
-    font-size: small;
-    font-weight: bold;
-    text-align: center;
-    color: white;
-    border-radius: 0.8em;
-  }
+.tag {
+  display: inline-block;
+  margin: 0.2em;
+  padding: 0.2em 0.8em;
+  text-transform: uppercase;
+  font-size: 0.6em;
+  font-weight: bold;
+  text-align: center;
+  border-radius: 0.4em;
 
+  transition: .8s transform cubic-bezier(.155,1.105,.295,1.12);
+}
 
-
+.tag:hover,
+.card-div:hover {
+  transform: scale(105%) translateY(-0.4vh);
+  z-index: 1;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,135 @@
  <script setup>
-    import HelloWorld from '@/components/HelloWorld.vue'
-    import CardItemVue from '../components/CardItem.vue';
-import CardItem from '../components/CardItem.vue';
-  </script>
+import HelloWorld from "@/components/HelloWorld.vue";
+import CardItem from "../components/CardItem.vue";
+</script>
+
+<script>
+export default {
+  components: { CardItem },
+  data() {
+    return {
+      cards: [
+        {
+          icon: "https://cdn-icons-png.flaticon.com/512/2285/2285537.png",
+          title: "Rocket Science",
+          description: "Sometimes it goes boom.",
+          tags_array: [
+            {
+              text: "Rocket Science",
+              style: "background-color: #008888;",
+            },
+            {
+              text: "Science",
+              style: "background-color: #008888;",
+            },
+            {
+              text: "Physics",
+              style: "background-color: #008888;",
+            },
+          ],
+        },
+        {
+          icon: "https://cdn-icons-png.flaticon.com/512/897/897406.png",
+          title: "Maths Basics",
+          description: "The Basics",
+          tags_array: [
+            {
+              text: "Maths",
+              style: "background-color: #008888;",
+            },
+             {
+              text: "Basics",
+              style: "background-color: #008888;",
+            },
+          ],
+        },
+         {
+          icon: "https://cdn-icons-png.flaticon.com/512/897/897406.png",
+          title: "Maths Advanced",
+          description: "Math is cool",
+          tags_array: [
+            {
+              text: "Maths",
+              style: "background-color: #008888;",
+            },
+             {
+              text: "Advanced",
+              style: "background-color: #008888;",
+            },
+          ],
+        },
+        {
+          icon: "https://cdn-icons-png.flaticon.com/512/3081/3081530.png",
+          title: "Science",
+          description: "Mad scientists are here",
+          tags_array: [
+            {
+              text: "Science",
+              style: "background-color: #008888;",
+            },
+          ],
+        },
+        {
+          icon: "https://cdn-icons-png.flaticon.com/512/2234/2234697.png",
+          title: "History",
+          description: "When did they do what ?",
+          tags_array: [
+            {
+              text: "History",
+              style: "background-color: #008888;",
+            },
+            {
+              text: "Boring",
+              style: "background-color: #008888;",
+            },
+          ],
+        },
+        {
+          icon: "https://cdn-icons.flaticon.com/png/512/3224/premium/3224018.png?token=exp=1648934341~hmac=5db3d934a1098a6b8f4fc0d932b124cb",
+          title: "Physics",
+          description: "It's going to get physical.",
+          tags_array: [
+            {
+              text: "Physics",
+              style: "background-color: #008888;",
+            },
+            {
+              text: "Not so Boring",
+              style: "background-color: #008888;",
+            },
+          ],
+        },
+        {
+          icon: "https://cdn-icons-png.flaticon.com/512/3655/3655592.png",
+          title: "Chemistry",
+          description: "We mix some stuff to make some other stuff.",
+          tags_array: [
+            {
+              text: "Chemistry",
+              style: "background-color: #008888;",
+            },
+            {
+              text: "Not so Boring",
+              style: "background-color: #008888;",
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
 
 <template>
-
   <div class="Home">
     <h1 msg="Build-a-cursus"></h1>
-    <CardItem></CardItem>
+    <CardItem v-for="card in cards" v-bind="card"></CardItem>
   </div>
 </template>
+
+<style scoped>
+.Home {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
