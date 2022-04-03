dc72581249bc0ccc1106bb4939d3a674f4c1c476
matthew-dreemurr <hadadmat@gmail.com>
2022-04-03 14:57:13 +0200

    Makefile push 2022-04-03 14:57:13

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 import wish_graph from '../views/wish_graph.vue'
 import CoursesView from '../views/CoursesView.vue'
 import Modify from '../views/Modify.vue'
